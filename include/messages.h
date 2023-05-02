e9ce283cd73fb7fdf97fc0b14512c4f8675c69cd
jason <jason2580134@outlook.com>
2023-05-01 23:09:13 -0700

    new headers

---
+++ include/messages.h
@@ -0,0 +1,20 @@
+#ifndef MESSAGES_H
+#define MESSAGES_H
+
+#define ARGS "neh"
+
+#define HELP_DESCR "SIM - SIMple Text Editor (%s)\n\
+\n\
+Usage: sim [arguments] [file ..]\n\
+     : sim [arguments]\n\
+\n\
+Arguments:\n\
+    -n                          new file\n\
+    -e                          edit existing file\n\
+    -r                          read existing file\n\
+    -l                          start at line number\n\
+    -h or --help                help message\n"
+
+#define INVALID_FLAG_DESCR "Use -h flag to see available options\n"
+
+#endif
